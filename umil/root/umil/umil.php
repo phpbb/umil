98d3da1e6c9603ce53fb1f99fd7c1146ea722d42
Nathan Guse <exreaction@phpbb.com>
2008-12-01 02:32:42 +0000

    Not sure if this fix will be kept for PostgreSQL.  PostgreSQL doesn't work when adding tables otherwise, but I have to find out if any other issues will come up because of it.

--- umil/root/umil/umil.php
+++ umil/root/umil/umil.php
@@ -2051,7 +2051,7 @@ class umil
 						$sql .= "DEFAULT nextval('{$table_name}_seq'),\n";
 
 						// Make sure the sequence will be created before creating the table
-						$sql .= "CREATE SEQUENCE {$table_name}_seq;\n\n" . $sql;
+						//$sql .= "CREATE SEQUENCE {$table_name}_seq;\n\n" . $sql;
 					}
 					else
 					{
