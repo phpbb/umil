c789dba9de8c053c85bd8d24f99fc545ca64790e
Nathan Guse <exreaction@phpbb.com>
2008-12-23 17:45:29 +0000

    Forgot to finish my comments on the version numbering

--- documentation/examples/umil_auto_example.php
+++ documentation/examples/umil_auto_example.php
@@ -51,7 +51,8 @@ $language_file = 'mods/umil_auto_example';
 * The array of versions and actions within each.
 * You do not need to order it a specific way (it will be sorted automatically), however, you must enter every version, even if no actions are done for it.
 *
-* You must use .  Only X.X.X (replacing X with a number).
+* You must use correct version numbering.  Unless you know exactly what you can use, only use X.X.X (replacing X with an integer).
+* The version numbering must otherwise be compatible with the version_compare function - http://php.net/manual/en/function.version-compare.php
 */
 $versions = array(
 	// Version 0.3.0
