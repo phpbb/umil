9d1d710a4dedab02019154accbc88af30592204d
Nathan Guse <exreaction@phpbb.com>
2009-04-18 03:35:53 +0000

    Oops, didn't think of parent being 0 with the previous commit.

--- umil/root/umil/umil.php
+++ umil/root/umil/umil.php
@@ -1253,7 +1253,7 @@ class umil
 
 			$parent = $data['parent_id'] = $row['module_id'];
 		}
-		else if (!$this->module_exists($class, false, $parent))
+		else if ($parent && !$this->module_exists($class, false, $parent))
 		{
 			return $this->umil_end('PARENT_NOT_EXIST');
 		}
