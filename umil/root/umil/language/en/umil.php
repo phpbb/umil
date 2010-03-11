cb9614f3e00eaefef713b34f6b305a442b7c3b88
Nathan Guse <exreaction@phpbb.com>
2010-03-11 02:19:30 +0000

    Formally adding this (the language strings): [Feature] Ability to create Permission roles.

--- umil/root/umil/language/en/umil.php
+++ umil/root/umil/language/en/umil.php
@@ -84,12 +84,16 @@ $lang = array_merge($lang, array(
 	'PERMISSION_ALREADY_EXISTS'		=> 'ERROR: Permission option %s already exists.',
 	'PERMISSION_NOT_EXIST'			=> 'ERROR: Permission option %s does not exist.',
 	'PERMISSION_REMOVE'				=> 'Removing permission option: %s',
+	'PERMISSION_ROLE_ADD'			=> 'Adding new permission role: %s',
+	'PERMISSION_ROLE_UPDATE'		=> 'Updating permission role: %s',
+	'PERMISSION_ROLE_REMOVE'		=> 'Removing permission role: %s',
 	'PERMISSION_SET_GROUP'			=> 'Setting permissions for the %s group.',
 	'PERMISSION_SET_ROLE'			=> 'Setting permissions for the %s role.',
 	'PERMISSION_UNSET_GROUP'		=> 'Unsetting permissions for the %s group.',
 	'PERMISSION_UNSET_ROLE'			=> 'Unsetting permissions for the %s role.',
 
-	'ROLE_NOT_EXIST'				=> 'Role does not exist',
+	'ROLE_ALREADY_EXISTS'			=> 'Permission role already exists.',
+	'ROLE_NOT_EXIST'				=> 'Permission role does not exist',
 
 	'SUCCESS'						=> 'Success',
 
